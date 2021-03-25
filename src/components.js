8c833e5b93f3205f0607e0d2dd067fcb06948a80
Mohammed <atefhamosa@gmail.com>
2021-03-26 00:33:30 +0200

    Fix linters

--- src/components.js
+++ src/components.js
@@ -1,41 +1,41 @@
 const text = (elem, text, className) => {
-    let t = document.createElement(`${elem}`);
-    t.textContent = `${text}`
-    t.classList.add(`${className}`)
-    return t
-}
+  let t = document.createElement(`${elem}`);
+  t.textContent = `${text}`;
+  t.classList.add(`${className}`);
+  return t;
+};
 
 function tabs() {
-    const navbar = text('ul','','navbar')
-    for (var i = 0; i < arguments.length; i+=1) {
-        let el = text('li',`${arguments[i]}`, 'nav-item');
-        navbar.appendChild(el)
-    }
-    return navbar
+  const navbar = text("ul", "", "navbar");
+  for (var i = 0; i < arguments.length; i += 1) {
+    let el = text("li", `${arguments[i]}`, "nav-item");
+    navbar.appendChild(el);
+  }
+  return navbar;
 }
 
 function menuItems(arr) {
-    const menu = text('section','','menu')
-    for (var i = 0; i < arr.length; i+=1) {
-        let card = text('div', '', 'card');
-        let img = text('img','', 'item-img');
-        img.setAttribute('alt',`${arr[i].img}`)
-        img.setAttribute('src',`./imgs/${arr[i].img}.png`)
-        let name = text('h4',`${arr[i].name}`, 'item-name');
-        let price = text('p',`${arr[i].price} -EGP`, 'item-price');
+  const menu = text("section", "", "menu");
+  for (var i = 0; i < arr.length; i += 1) {
+    let card = text("div", "", "card");
+    let img = text("img", "", "item-img");
+    img.setAttribute("alt", `${arr[i].img}`);
+    img.setAttribute("src", `./imgs/${arr[i].img}.png`);
+    let name = text("h4", `${arr[i].name}`, "item-name");
+    let price = text("p", `${arr[i].price} -EGP`, "item-price");
 
-        card.appendChild(img)
-        card.appendChild(name)
-        card.appendChild(price)
-        menu.appendChild(card);
-    }
-    return menu
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(price);
+    menu.appendChild(card);
+  }
+  return menu;
 }
 
 function createMain() {
-    const main = text('div', '', 'main')
-    main.id = 'main'
-    return main
+  const main = text("div", "", "main");
+  main.id = "main";
+  return main;
 }
 
-export { text, tabs, menuItems, createMain }
+export { text, tabs, menuItems, createMain };
